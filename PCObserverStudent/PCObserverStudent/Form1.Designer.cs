9399a5043e9e224b81aebf05f34291cf0ce3fbf1
wsx9721 <924708077@qq.com>
2018-08-13 16:35:12 +0800

    form

--- PCObserverStudent/PCObserverStudent/Form1.Designer.cs
+++ PCObserverStudent/PCObserverStudent/Form1.Designer.cs
@@ -39,14 +39,14 @@
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("宋体", 12F);
-            this.textBox1.Location = new System.Drawing.Point(239, 42);
+            this.textBox1.Location = new System.Drawing.Point(189, 30);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(301, 26);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(239, 78);
+            this.textBox2.Location = new System.Drawing.Point(189, 66);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
@@ -57,7 +57,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("宋体", 12F);
-            this.label1.Location = new System.Drawing.Point(145, 42);
+            this.label1.Location = new System.Drawing.Point(95, 30);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(88, 16);
             this.label1.TabIndex = 2;
@@ -67,7 +67,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("宋体", 12F);
-            this.label2.Location = new System.Drawing.Point(145, 78);
+            this.label2.Location = new System.Drawing.Point(95, 66);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(88, 16);
             this.label2.TabIndex = 3;
@@ -75,16 +75,17 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(239, 322);
+            this.button1.Location = new System.Drawing.Point(189, 310);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(143, 33);
             this.button1.TabIndex = 4;
             this.button1.Text = "连接";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(397, 322);
+            this.button2.Location = new System.Drawing.Point(347, 310);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(143, 33);
             this.button2.TabIndex = 5;
@@ -95,7 +96,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(709, 367);
+            this.ClientSize = new System.Drawing.Size(652, 367);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
--- PCObserverStudent/PCObserverStudent/Form1.cs
+++ PCObserverStudent/PCObserverStudent/Form1.cs
@@ -16,5 +16,10 @@ namespace PCObserverStudent
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
